e543ba0032458847eb88a2a402888d60c7f5f344
nandaniya480-BH <132910458+nandaniya480-BH@users.noreply.github.com>
2023-05-09 18:41:14 +0530

    Update BlogProvider.php

--- src/Providers/BlogProvider.php
+++ src/Providers/BlogProvider.php
@@ -8,8 +8,43 @@ class BlogProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
-        $this->loadMigrationsFrom(__DIR__ . '/../Database/migrations');
+
+        if ($this->app->runningInConsole()) {
+            $this->registerPublishing();
+        }
+
+        $this->registerResources();
+    }
+
+    /**
+     * Register the package resources.
+     *
+     * @return void
+     */
+    private function registerResources()
+    {
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
         $this->loadViewsFrom(__DIR__ . '/../resources/views', 'Blog');
+
+        $this->registerRoutes();
+    }
+
+    /**
+     * Register the package routes.
+     *
+     * @return void
+     */
+    protected function registerRoutes()
+    {
+        // Route::group($this->routeConfiguration(), function () {
+        $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
+        // });
+    }
+
+    protected function registerPublishing()
+    {
+        $this->publishes([
+            __DIR__ . '/Console/stubs/PressServiceProvider.stub' => app_path('Providers/PressServiceProvider.php'),
+        ], 'press-provider');
     }
 }
