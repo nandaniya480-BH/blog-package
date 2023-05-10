6fa2f30857c5c74b213ea673c7aa920f172bbbe5
nandaniya480-BH <govindn.brainerhub@gmail.com>
2023-05-10 13:12:02 +0530

    update

--- src/Providers/BlogProvider.php
+++ src/Providers/BlogProvider.php
@@ -6,13 +6,20 @@ use Illuminate\Support\ServiceProvider;
 
 class BlogProvider extends ServiceProvider
 {
+    // public function boot()
+    // {
+    //     if ($this->app->runningInConsole()) {
+    //         $this->registerPublishing();
+    //     }
+
+    //     $this->registerResources();
+    // }
+
     public function boot()
     {
-        if ($this->app->runningInConsole()) {
-            $this->registerPublishing();
-        }
-
-        $this->registerResources();
+        $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
+        $this->loadMigrationsFrom(__DIR__ . '/../Database/migrations');
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'Blog');
     }
 
     /**
@@ -20,28 +27,28 @@ class BlogProvider extends ServiceProvider
      *
      * @return void
      */
-    private function registerResources()
-    {
-        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
-        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'Blog');
+    // private function registerResources()
+    // {
+    //     $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+    //     $this->loadViewsFrom(__DIR__ . '/../resources/views', 'Blog');
 
-        $this->registerRoutes();
-    }
+    //     $this->registerRoutes();
+    // }
 
-    /**
-     * Register the package routes.
-     *
-     * @return void
-     */
-    protected function registerRoutes()
-    {
-        $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
-    }
+    // /**
+    //  * Register the package routes.
+    //  *
+    //  * @return void
+    //  */
+    // protected function registerRoutes()
+    // {
+    //     $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
+    // }
 
-    protected function registerPublishing()
-    {
-        $this->publishes([
-            __DIR__ . '/Console/stubs/BlogProvider.php' => app_path('Providers/BlogProvider.php'),
-        ], 'blog-provider');
-    }
+    // protected function registerPublishing()
+    // {
+    //     $this->publishes([
+    //         __DIR__ . '/Console/stubs/BlogProvider.php' => app_path('Providers/BlogProvider.php'),
+    //     ], 'blog-provider');
+    // }
 }
