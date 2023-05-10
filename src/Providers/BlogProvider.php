bdc40f3755c2073c30e85eaaab92b7748b51bc60
nandaniya480-BH <govindn.brainerhub@gmail.com>
2023-05-10 17:14:10 +0530

    update

--- src/Providers/BlogProvider.php
+++ src/Providers/BlogProvider.php
@@ -6,49 +6,10 @@ use Illuminate\Support\ServiceProvider;
 
 class BlogProvider extends ServiceProvider
 {
-    // public function boot()
-    // {
-    //     if ($this->app->runningInConsole()) {
-    //         $this->registerPublishing();
-    //     }
-
-    //     $this->registerResources();
-    // }
-
     public function boot()
     {
         $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
         $this->loadMigrationsFrom(__DIR__ . '/../Database/migrations');
         $this->loadViewsFrom(__DIR__ . '/../resources/views', 'Blog');
     }
-
-    /**
-     * Register the package resources.
-     *
-     * @return void
-     */
-    // private function registerResources()
-    // {
-    //     $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
-    //     $this->loadViewsFrom(__DIR__ . '/../resources/views', 'Blog');
-
-    //     $this->registerRoutes();
-    // }
-
-    // /**
-    //  * Register the package routes.
-    //  *
-    //  * @return void
-    //  */
-    // protected function registerRoutes()
-    // {
-    //     $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
-    // }
-
-    // protected function registerPublishing()
-    // {
-    //     $this->publishes([
-    //         __DIR__ . '/Console/stubs/BlogProvider.php' => app_path('Providers/BlogProvider.php'),
-    //     ], 'blog-provider');
-    // }
-}
+} 
